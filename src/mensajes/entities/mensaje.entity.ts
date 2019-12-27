5dbb3e13dc4806c92ae3f104c702a8932acb8851
sebastian <sebaqd91@gmail.com>
2019-12-27 12:28:31 -0300

    initial commit

---
+++ src/mensajes/entities/mensaje.entity.ts
@@ -0,0 +1,15 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity()
+
+export class Mensaje {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    nick: string;
+
+    @Column()
+    mensaje: string;
+}
